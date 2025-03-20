785dfe8248607fd242f7f622358f61a2697bfc4b
LaloAleman18 <20233tn226@utez.edu.mx>
2025-03-20 00:09:44 -0600

    Agregacion de Rutas

--- navigation/UserNavigator.js
+++ navigation/UserNavigator.js
@@ -4,6 +4,8 @@ import UserHomeScreen from "../screens/UserHomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import UserListaMascotasScreen from "../screens/UserListaMascotas";
 import UserDetallesMascotaScreen from "../screens/UserDetallesMascotas";
+import UserFavoritosScreen from "../screens/UserFavoritosScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -12,12 +14,16 @@ const UserStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="UserListaMascotasScreen" component={UserListaMascotasScreen} />
     <Stack.Screen name="UserDetallesMascotaScreen" component={UserDetallesMascotaScreen} />
+    <Stack.Screen name="UserFavoritosScreen" component={UserFavoritosScreen} />
+
   </Stack.Navigator>
 );
+
 const UserNavigator = () => (
   <Drawer.Navigator>
     <Drawer.Screen name="Inicio" component={UserStack} />
-    <Drawer.Screen name="Perfil" component={ProfileScreen} />
+    <Drawer.Screen name="Favoritos" component={UserFavoritosScreen} />
+    <Drawer.Screen name="Configuracion" component={SettingsScreen} />
   </Drawer.Navigator>
 );
 
